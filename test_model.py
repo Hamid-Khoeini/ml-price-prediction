83756b6bd1c3c5e8981d261355d27f67c5e98014
Hamid Khoeini <130043932+Hamid-Khoeini@users.noreply.github.com>
2025-11-14 10:59:49 +0330

    Update test_model.py

--- test_model.py
+++ test_model.py
@@ -1,3 +1,4 @@
+# test_model.py
 import joblib
 import pandas as pd
 from sklearn.datasets import fetch_california_housing
@@ -5,12 +6,11 @@ from sklearn.datasets import fetch_california_housing
 # بارگذاری مدل
 model = joblib.load("models/housing_model.pkl")
 
-# یه نمونه تست
+# نمونه تست
 housing = fetch_california_housing(as_frame=True)
 X_sample = housing.frame.drop("MedHouseVal", axis=1).iloc[:5]
-
 pred = model.predict(X_sample)
-print("پیش‌بینی ۵ خانه اول:", pred)
 
+print("پیش‌بینی ۵ خانه اول:", pred)
 assert len(pred) == 5
-print("تست موفق! مدل درست کار می‌کنه ✅")
+print("تست موفق! مدل درست کار می‌کنه")
